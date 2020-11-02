9747b2d17bc21c86cb680c1619e502f6f4aee848
Dominik Krakau <plutomars_de@yahoo.de>
2020-11-02 18:59:53 +0100

    Connection of react components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import ContractVisualisation from './modules/ContractVisualisation';
 import GraphCreator from './modules/GraphCreator';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  state = {
+    address: null
+  };
+
+
+  render() {
+
+    const graph = (this.state.address !== null) ? <GraphCreator address={this.state.address} /> : null;
+
+    return (
+      <div className="App">
         <h1 className="title">Visualisation of Bloxsberg-Network</h1>
-        <ContractVisualisation />
-        <div id="graph_box">
-          <GraphCreator />
+        <div id="input">
+          <input type="text" id="address" placeholder="Address..." /><input type="submit" id="button" value="Submit" onClick={() => {
+
+            let addr = document.getElementById("address").value;
+            
+            if (addr === "") {
+              alert("Enter an Address first.");
+            } else if (!addr.match("^0x([A-Fa-f0-9]{40})$")) {
+              alert("Your input is not an address.");
+            } else {
+              this.setState({ address: addr});
+            }
+
+          }} />
         </div>
-    </div>
-  );
+        <div id="graph_box">
+          {graph}
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/modules/GraphCreator.js
+++ src/modules/GraphCreator.js
@@ -6,99 +6,114 @@ import DataCollector from './DataCollector';
 class GraphCreator extends React.Component {
     constructor(props) {
         super(props);
-        this.data = new DataCollector().getData("0x0d81b4b37EddC9C262198A82B51470aa1eBa14C4");  // 0x4945d63B509e137b0293Bd958cf97B61996c0fB9       // 0xC91a26a4351c6b351Cc2231e9c7bE7dd7D4a7036
-
-        console.log(this.data);
+        console.log("Consturctor.")
+        this.data = new DataCollector().getData(props.address); // new DataCollector().getData("0x0d81b4b37EddC9C262198A82B51470aa1eBa14C4");  // 0x4945d63B509e137b0293Bd958cf97B61996c0fB9       // 0xC91a26a4351c6b351Cc2231e9c7bE7dd7D4a7036
     }
 
-    componentDidMount() {
+    createGraph() {
+        if (this.data !== null) {
 
-        this.data.then((promise) => {
+            this.data.then((promise) => {
 
-            console.log(promise);
+                console.log(promise);
 
-            let height = 800,
-                width = 800;
+                let height = 800,
+                    width = 800;
 
-            let links = promise;
+                let links = promise;
 
-            // create empty nodes array
-            let nodes = {};
+                // create empty nodes array
+                let nodes = {};
 
-            // compute nodes from links data
-            links.forEach(function (link) {
-                console.log(link.source);
-                link.source = nodes[link.source] ||
-                    (nodes[link.source] = { source: link.source });
-                link.target = nodes[link.target] ||
-                    (nodes[link.target] = { target: link.target });/*
+                // compute nodes from links data
+                links.forEach(function (link) {
+                   // console.log("link: " + link.source);
+                    link.source = nodes[link.source] ||
+                        (nodes[link.source] = { source: link.source });
+                    link.target = nodes[link.target] ||
+                        (nodes[link.target] = { target: link.target });/*
             link.count = nodes[link.count] || 
                 (nodes[link.count] = { count: link.count });
             link.type = nodes[link.type] || 
                 (nodes[link.type] = { type: link.type });*/
-            });
-
-            // add a SVG to the body for our viz
-            let svg = d3.select('#container').append('svg')
-                .attr('width', width)
-                .attr('height', height);
-
-
-            // use the force
-            let force = d3.layout.force() //build the layout
-                .size([width, height]) //specified earlier
-                .nodes(d3.values(nodes)) //add nodes
-                .links(links) //add links
-                .on("tick", tick) //what to do
-                .linkDistance(200) //set for proper svg size
-                .charge(function (d) {
-                    var charge = -500;
-                    if (d.index === 0) charge = 10 * charge;
-                    return charge;
-                })
-                .start(); //kick the party off!
-
-            // add the links
-            let link = svg.selectAll('.link')
-                .data(links)
-                .enter().append('line')
-                .attr('class', 'link');
-
-            // add the nodes
-            let node = svg.selectAll('.node')
-                .data(force.nodes()) //add
-                .enter().append('circle')
-                .style("fill", function (d) {
-                    if (d.type === "contract") {
-                        return "orange";
-                    }
-                })
-                .attr('class', 'node')
-                .attr('r', width * 0.02) //radius of circle
-                .on("click", function (d) {
-                    console.log(d.source);
-                    document.getElementById("info").innerHTML = d.source;
                 });
 
-            function tick(e) {
+                document.getElementById("container").innerHTML = "";
+                // add a SVG to the body for our viz
+                let svg = d3.select('#container').append('svg')
+                    .attr('width', width)
+                    .attr('height', height);
 
-                node.attr('cx', function (d) { return d.x; })
-                    .attr('cy', function (d) { return d.y; })
-                    .call(force.drag);
 
-                link.attr('x1', function (d) { return d.source.x; })
-                    .attr('y1', function (d) { return d.source.y; })
-                    .attr('x2', function (d) { return d.target.x; })
-                    .attr('y2', function (d) { return d.target.y; });
+                // use the force
+                let force = d3.layout.force() //build the layout
+                    .size([width, height]) //specified earlier
+                    .nodes(d3.values(nodes)) //add nodes
+                    .links(links) //add links
+                    .on("tick", tick) //what to do
+                    .linkDistance(200) //set for proper svg size
+                    .charge(function (d) {
+                        var charge = -500;
+                        if (d.index === 0) charge = 10 * charge;
+                        return charge;
+                    })
+                    .start(); //kick the party off!
 
-            }
+                // add the links
+                let link = svg.selectAll('.link')
+                    .data(links)
+                    .enter().append('line')
+                    .attr('class', 'link');
 
-        }).catch((err) => {
-            console.error(err);
-        });
+                // add the nodes
+                let node = svg.selectAll('.node')
+                    .data(force.nodes()) //add
+                    .enter().append('circle')
+                    .style("fill", function (d) {
+                        if (d.type === "contract") {
+                            return "orange";
+                        }
+                    })
+                    .attr('class', 'node')
+                    .attr('r', width * 0.02) //radius of circle
+                    .on("click", function (d) {
+                        console.log(d.source);
+                        document.getElementById("info").innerHTML = d.source;
+                    });
+
+                function tick(e) {
+
+                    node.attr('cx', function (d) { return d.x; })
+                        .attr('cy', function (d) { return d.y; })
+                        .call(force.drag);
+
+                    link.attr('x1', function (d) { return d.source.x; })
+                        .attr('y1', function (d) { return d.source.y; })
+                        .attr('x2', function (d) { return d.target.x; })
+                        .attr('y2', function (d) { return d.target.y; });
+
+                }
+
+            }).catch((err) => {
+                alert(err);
+                console.error(err);
+            });
+        } 
+    }
+
+    componentDidMount() {
+        console.log("Component did mount.");
+        this.createGraph();
+    }
+
+    componentWillUpdate(nextProps) {
+        console.log("Component will update.");
+        this.data = new DataCollector().getData(nextProps.address);
+        this.createGraph();
     }
 
     render() {
+
         return (<div>
             <div id="info"></div>
             <div id="container"></div>
