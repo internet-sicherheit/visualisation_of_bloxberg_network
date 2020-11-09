28491e8e6234744cf87f691ff8fd26cb3a5826d6
Dominik Krakau <plutomars_de@yahoo.de>
2020-11-09 15:24:55 +0100

    Fetching Http response to d3 graph

--- src/App.js
+++ src/App.js
@@ -6,35 +6,33 @@ class App extends Component {
 
   state = {
     page: 0,
-    offset: 0
+    offset: 0,
+    stage: 0
   };
 
   render() {
 
-    let graph = (this.state.page !== 0) ? <GraphCreator page={this.state.page} offset={this.state.offset} /> : null;
+    let graph = (this.state.page !== 0) ? <GraphCreator page={this.state.page} offset={this.state.offset} stage={this.state.stage} /> : null;
 
     const maxPage = 100;
     const maxOffset = 1800;
+    const maxStage = 5;
 
-    const pageSelectedValue = 1;
-    const offsetSelectedValue = 3;
+    const pageSelectedValue = 2;
+    const offsetSelectedValue = 1;
+    const stageSelectedValue = 1;
 
     const pageSelectOptions = [];
     for(let i = 1; i <= maxPage; i++) {
-      if(i === pageSelectedValue) {
-        pageSelectOptions.push(<option key={i} value={i} selected>{i}</option>);
-      } else {
-        pageSelectOptions.push(<option key={i} value={i}>{i}</option>);
-      }
+      pageSelectOptions.push(<option key={i} value={i}>{i}</option>);
     }
-
     const offsetSelectOptions = [];
     for(let i = 1; i <= maxOffset; i++) {
-      if(i === offsetSelectedValue) {
-        offsetSelectOptions.push(<option key={i} value={i} selected>{i}</option>);
-      } else {
-        offsetSelectOptions.push(<option key={i} value={i}>{i}</option>);
-      }
+      offsetSelectOptions.push(<option key={i} value={i}>{i}</option>);
+    }
+    const stageSelectOptions = [];
+    for(let i = 1; i <= maxStage; i++) {
+      stageSelectOptions.push(<option key={i} value={i}>{i}</option>);
     }
 
     return (
@@ -42,22 +40,28 @@ class App extends Component {
         <h1 className="title">Visualisation of Bloxsberg-Network</h1>
         <div id="input">
           <label>Page:</label>
-          <select id="page_selection">
+          <select id="page_selection" defaultValue={pageSelectedValue}>
             {pageSelectOptions}
           </select>
           <label>Offset:</label>
-          <select id="offset_selection">
+          <select id="offset_selection" defaultValue={offsetSelectedValue}>
             {offsetSelectOptions}
+          </select>
+          <label>Stage:</label>
+          <select id="stage_selection" defaultValue={stageSelectedValue}>
+            {stageSelectOptions}
           </select>
           <input type="submit" id="button" value="Start" onClick={() => {
 
             let page = document.getElementById("page_selection").value;
             let offset = document.getElementById("offset_selection").value;
+            let stage = document.getElementById("stage_selection").value;
 
             console.log("page -> " + page);
             console.log("offset -> " + offset);
+            console.log("stage -> " + stage);
 
-            this.setState({ page: page, offset: offset});
+            this.setState({ page: page, offset: offset, stage: stage});
             
           }} />
         </div>
--- src/modules/DataCollector.js
+++ src/modules/DataCollector.js
@@ -6,34 +6,111 @@ class DataCollector extends React.Component {
         super(props);
     }
 
-    async getData(address, mode) {
+    async getData(page, offset, stage) {
 
-        const Http = new XMLHttpRequest();
-        const url = 'https://blockexplorer.bloxberg.org/api/api?module=account&action=txlist&address=' + address;  
-                                                                                                                                                   
-        let transactions = await fetch(url).then(response => response.json());
+        // get contract list
+        let contractList = await this.getContractList(page, offset, stage);
+        console.log("Get contractlist:");
+        console.log(contractList);
 
-        let result = null;
+        // get transactions from contract list
+        let transactionList = await this.getTransactionFromContractList(contractList, stage);
+        console.log("Get transactionlist from contractlist:");
+        console.log(transactionList);
 
-        switch(mode) {
-            case "contract":
-                result = this.parseHTTPResponseForContract(transactions.result);
-                break;
-            case "account":
-                result = this.parseHTTPResponseForNetwork(transactions.result);
-                break;
-        }
 
-        return result;
+        let a = [];
+
+
+        let b = [
+            {source: "e", target: "b"},
+            {source: "f", target: "b"},
+            {source: "g", target: "b"},
+            {source: "h", target: "b"},
+        ]
+
+        Array.prototype.push.apply(a,b)
+
+        console.log(a);
+
+        return transactionList;
+
+        // iterate over accounts
+        //for (let i = 0; i < offset; i++) {
+
+        //}
     }
 
-    parseHTTPResponseForContract(response) {
+    async getContractList(page, offset) {
+
+        const url = 'https://blockexplorer.bloxberg.org/api/api?module=contract&action=listcontracts&page=' + page + '&offset=' + offset;
+
+        let contractList = await fetch(url).then(response => response.json());
+
+        return contractList.result;
+    }
+
+    async getTransactionList(contract) {
+
+        let url = 'https://blockexplorer.bloxberg.org/api/api?module=account&action=txlist&address=' + contract;
+
+        let transactionList = await fetch(url).then(response => response.json());
+
+        console.log("getTransactionList: " + transactionList);
+
+        return null;
+    }
+
+    async getTransactionFromContractList(response) {  // 0x97c314818fbe22b4b5d5Ea75E52E726271aFAE3b  
+
+        let transactionList = [];
+
+        response.forEach(async function (contract) {
+            let url = 'https://blockexplorer.bloxberg.org/api/api?module=account&action=txlist&address=';
+            //console.log("ContractAddress: " + contract.Address);
+            url = url + contract.Address;
+
+            let arrays = await fetch(url).then(response => response.json());
+            let transactions = [];
+
+            let length = Object.keys(arrays.result).length;
+            let array = arrays.result;
+
+            for (let i = 0; i < length; i++) {
+
+                if (transactions.length === 0) {
+                    transactions.push({ source: array[i].from, target: array[i].to });
+
+                }
+
+                let found = 0;
+                for (let j = 0; j < transactions.length; j++) {
+
+                    if (transactions[j].source === array[i].from && transactions[j].target === array[i].to) {
+                        found = 1;
+                    }
+
+                }
+                if (found === 0 && array[i].to !== "") {
+                    transactions.push({ source: array[i].from, target: array[i].to });
+                }
+
+            }
+            Array.prototype.push.apply(transactionList, transactions);
+            //transactionList.push(transactions);
+        });
+        return transactionList;
+    }
+
+    parseHTTPResponse(response) {
+
+        console.log("parseHTTPresponse");
 
         let transactions = [];
 
         let length = Object.keys(response).length;
         let array = response;
-        
+
         /*
         for(let i = 0; i < length; i++) {
             transactions.push({ source: array[i].from, target: array[i].to });
@@ -71,37 +148,6 @@ class DataCollector extends React.Component {
                     }*/
 
         return transactions;
-    }
-
-    parseHTTPResponseForNetwork(response) {  // 0x97c314818fbe22b4b5d5Ea75E52E726271aFAE3b  
-        console.log("network vis");
-
-        let page = 50;
-        let offset = 120;
-
-        const Http = new XMLHttpRequest();
-        const url = 'https://blockexplorer.bloxberg.org/api/api?module=contract&action=listcontracts&page=' + page + '&offset=' + offset;  
-                                                                                                                                                   
-        let contracts = fetch(url).then(response => response.json());
-
-
-        contracts.then((promise) => {
-
-            let list = promise.result;
-            let contractList = [];
-
-            list.forEach(function(contract) {
-                contractList.push({contractAddress: contract.Address});
-            });
-
-            console.log("contractList");
-            console.log(contractList);
-
-
-        }).catch((err) => {
-            console.error(err);
-        });
-        
     }
 }
 
