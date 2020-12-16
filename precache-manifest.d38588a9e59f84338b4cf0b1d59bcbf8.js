be2de983d1b31e427ffb0b2439c57736dee573c5
Dominik <dominik.krakau@studmail.w-hs.de>
2020-12-16 17:59:30 +0100

    Updates

---
+++ precache-manifest.d38588a9e59f84338b4cf0b1d59bcbf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62274747ed33af44db6d767791167ecd",
+    "url": "/visualisation_of_bloxberg_network/index.html"
+  },
+  {
+    "revision": "6e432cc39f6ff5d4fe23",
+    "url": "/visualisation_of_bloxberg_network/static/css/main.7a794d0c.chunk.css"
+  },
+  {
+    "revision": "2eb93a2d543eecc24fcb",
+    "url": "/visualisation_of_bloxberg_network/static/js/2.035a6f29.chunk.js"
+  },
+  {
+    "revision": "2a718716713a14313a75e0453a825111",
+    "url": "/visualisation_of_bloxberg_network/static/js/2.035a6f29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e432cc39f6ff5d4fe23",
+    "url": "/visualisation_of_bloxberg_network/static/js/main.e23af418.chunk.js"
+  },
+  {
+    "revision": "7c0b9efae0f16893e699",
+    "url": "/visualisation_of_bloxberg_network/static/js/runtime-main.d013d7be.js"
+  }
+]);
